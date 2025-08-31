30528d81f5037e89cfbb5fe6c1e33a801548ef1d
Chikwa235 <chisapachikwa25@gmail.com>
2025-08-31 21:38:00 +0200

    Import components, Leaflet, and styles in App.js

--- food-expiry-tracker/src/App.jsx
+++ food-expiry-tracker/src/App.jsx
@@ -1,15 +1,9 @@
-import { useState } from 'react'
 
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <h1>Food Expiry Tracker</h1>
-    </>
-  )
-}
-
-export default App
+import React, { useEffect, useState, useRef } from 'react';
+import L from "leaflet";
+import "leaflet/dist/leaflet.css";
+import FoodItem from './Components/FoodItem';
+import DonateForm from './Components/DonateForm';
+import NGOCard from './Components/NGOCard';
+import Chart from './Components/Chart';
+import './App.css';
